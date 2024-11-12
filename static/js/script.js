852af018e8f08170dd61fe5cc71c28fa7ce68aa6
Kuriyama9590 <kuriyama9590@gmail.com>
2024-11-12 11:05:17 +0800

    新增视频导航页面，内链待完善

--- static/js/script.js
+++ static/js/script.js
@@ -19,3 +19,25 @@ function createCheer() {
         }, i * 100);
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {/*导航栏函数 */
+    const navButton = document.querySelector('.button-top');
+    const sidebar = document.querySelector('.sidebar');
+    const navContainer = document.querySelector('.nav-container');
+
+    navButton.addEventListener('mouseenter', function() {
+        sidebar.style.display = 'block'; // 鼠标进入按钮时显示导航栏
+    });
+
+    sidebar.addEventListener('mouseenter', function() {
+        sidebar.style.display = 'block'; // 鼠标进入导航栏时保持显示
+    });
+
+    sidebar.addEventListener('mouseleave', function() {
+        sidebar.style.display = 'none'; // 鼠标离开导航栏时隐藏
+    });
+
+    navContainer.addEventListener('mouseleave', function() {
+        sidebar.style.display = 'none'; // 鼠标离开box时隐藏
+    });
+});
